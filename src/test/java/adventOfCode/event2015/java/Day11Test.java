4f23bdf14a7b69bc8a178e0e2a0179b78f92f80c
igor.dundic <igor.dundic@assentis.com>
2020-05-23 18:07:46 +0200

    11

--- src/test/java/adventOfCode/event2015/java/Day11Test.java
+++ src/test/java/adventOfCode/event2015/java/Day11Test.java
@@ -2,6 +2,7 @@ package adventOfCode.event2015.java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +11,10 @@ public class Day11Test {
 
     private String input;
 //    private String[] alphabetLarge = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    private Character[] alphabetSmall = new Character[] {  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private char[] alphabetSmall = new char[] {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z'
+        };
     private List<Character> alphabetList;
 
 
@@ -37,17 +41,19 @@ public class Day11Test {
     private String passwordGenerator(String oldPassword) {
         byte[] newPassword = oldPassword.getBytes();
 
-        // niz od 3 slova rastuce
-        // ne sme imati i, o, l
-        // mora da ima dva nepreklapajuca para
-        // sifra se generise inkrementalno unazad od najmanjeg slova (kao sa brojevima)
-
         for (int i = newPassword.length - 1; i >= 0; i--) {
             newPassword = incrementCharacter(i, newPassword);
+            System.out.println(new String(newPassword));
+            Scanner myObj = new Scanner(System.in);
 
-            boolean hasTwoPairs = true;
-            boolean hasThreeInARow = true;
-            boolean hasNotIOL = true;
+            // has two pairs
+            boolean hasTwoPairs = checkHasTwoPairs(newPassword);
+
+            // has three in a row
+            boolean hasThreeInARow = checkHasThreeInARow(newPassword);
+
+            // has not IOL
+            boolean hasNotIOL = checkHasNotIOL(newPassword);
 
             if (hasTwoPairs && hasThreeInARow && hasNotIOL) {
                 break;
@@ -59,15 +65,50 @@ public class Day11Test {
         return new String(newPassword);
     }
 
+    private boolean checkHasTwoPairs(byte[] password) {
+        boolean hasTwoPairs = false;
+        for (int i = 0; i < password.length - 3; i++) {
+            if (password[i] == password[i + 1]) {
+                for (int j = i + 2; j < password.length - 1; j++) {
+                    if (password[j] == password[j + 1]) {
+                        hasTwoPairs = true;
+                    }
+                }
+            }
+        }
+        return hasTwoPairs;
+    }
+
+    private boolean checkHasThreeInARow(byte[] password) {
+        return false;
+    }
+
+    private boolean checkHasNotIOL(byte[] password) {
+        return false;
+    }
+
     private byte[] incrementCharacter(int letterPosition, byte[] byteArray) {
-//        int position = alphabetList.indexOf(character);
-//        return position == 25 ? 'a' : alphabetList.get(position + 1);
         byte[] newByteArray = byteArray;
 
         int i = letterPosition;
-        while(i > 0) {
-            
-            i--;
+        for (int j = 0; j < alphabetSmall.length; j++) {
+            if (newByteArray[i] == 'z') {
+                newByteArray[i] = 'a';
+                if (i > 0) {
+                    newByteArray = incrementCharacter(letterPosition - 1, newByteArray);
+                } else {
+                    byte[] longerByteArray = new byte[byteArray.length + 1];
+                    longerByteArray[0] = 'a';
+                    for (int k = 1; k < longerByteArray.length; k++) {
+                        longerByteArray[k] = newByteArray[k - 1];
+                    }
+                    newByteArray = longerByteArray;
+                }
+                break;
+            } else if (newByteArray[i] == alphabetSmall[j]) {
+                newByteArray[i] = (byte) alphabetSmall[j + 1];
+                break;
+            }
         }
 
         return newByteArray;
