dc82efab70e3d11cd15d4ff3b7f4eeff3bcb1ca4
igor.dundic <igor.dundic@assentis.com>
2020-09-22 10:15:01 +0200

    12 finish + 13 begin

--- src/test/java/adventOfCode/event2015/java/Day12Test.java
+++ src/test/java/adventOfCode/event2015/java/Day12Test.java
@@ -1,7 +1,18 @@
 package adventOfCode.event2015.java;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,32 +26,69 @@ public class Day12Test {
     private String input;
 
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException {
         input = AdventOfCodeUtility.loadInputDataToString(
             "C:\\Dev\\AdventOfCode\\src\\test\\java\\adventOfCode\\event2015\\resources\\Day12Input.txt");
     }
 
     @Test
     public void part1() {
+        System.out.println(extractSum(input));
+    }
+
+    private Integer extractSum(String input) {
         Pattern p = Pattern.compile("-?\\d+");
         Matcher m = p.matcher(input);
         Integer sum = 0;
+
         while(m.find()) {
             sum = sum + parseInt(m.group());
         }
-        System.out.println(sum);
+
+        return sum;
     }
 
     @Test
     public void part2() {
-        Pattern p = Pattern.compile("-?\\d+");
-        Matcher m = p.matcher(input);
-        Integer sum = 0;
-        while(m.find()) {
-            sum = sum + parseInt(m.group());
-            System.out.println(m.group());
-        }
+        JsonElement json =
+            JsonParser.parseReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+        System.out.println(json);
+        double sum = traverse(json);
         System.out.println(sum);
     }
+
+    private boolean isRed(JsonElement element) {
+        return element.isJsonPrimitive() && "red".equalsIgnoreCase(element.getAsString());
+    }
+
+    public double traverse(JsonElement node) {
+        double sum = 0;
+        if (node.isJsonArray()) {
+            JsonArray jsonArray = node.getAsJsonArray();
+            for (JsonElement child : jsonArray) {
+                sum += traverse(child);
+            }
+        } else if (node.isJsonObject()) {
+            JsonObject jsonObject = node.getAsJsonObject();
+            Set<Map.Entry<String, JsonElement>> entries = jsonObject.entrySet();
+            for (Map.Entry<String, JsonElement> entry : entries) {
+                if (isRed(entry.getValue())) {
+                    return sum; // skip the whole parent node
+                }
+            }
+            for (Map.Entry<String, JsonElement> entry : entries) {
+                sum += traverse(entry.getValue());
+            }
+        } else if (node.isJsonPrimitive()) {
+            JsonPrimitive jsonPrimitive = node.getAsJsonPrimitive();
+            if (jsonPrimitive.isNumber()) {
+                sum += jsonPrimitive.getAsDouble();
+            }
+        }
+
+        return sum;
+    }
+
+
 
 }
---
+++ src/test/java/adventOfCode/event2015/java/Day13Test.java
@@ -0,0 +1,26 @@
+package adventOfCode.event2015.java;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import adventOfCode.utils.AdventOfCodeUtility;
+
+public class Day13Test {
+
+    @Before
+    public void setUp() {
+        String input = AdventOfCodeUtility.loadInputDataToString("C:\\Dev\\AdventOfCode\\src\\com\\adventOfCode\\event2015\\resources\\Day1Input.txt");
+
+        System.out.println(input);
+    }
+
+    @Test
+    public void part1() {
+
+    }
+
+    @Test
+    public void part2() {
+
+    }
+}
